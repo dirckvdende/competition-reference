9adc1fd801b67e6e59ec0e6f51eed08cda9dc981
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-09-19 14:54:33 +0200

    Updated logo, team name and custom hashes

--- src/misc/hash.cc
+++ src/misc/hash.cc
@@ -1,17 +1,19 @@
-// Basic implementation of rolling hash, but without
-// the extra functionality of adding and removing items
-// easily
-struct vihash {
-    size_t operator()(const vi &v) const {
-        static const ll p = 1031;
-        static const ll M = 33554467;
-        ll pw = 1;
-        ll r = 0;
-        for (const ll &i : v) {
-            r += (i % M) * pw;
-            r %= M;
-            pw *= p, pw %= M;
+namespace std {
+    template<class T>
+    struct hash<vector<T>> {
+        size_t operator()(const vector<T> &v) const {
+            hash<T> ht; hash<size_t> h;
+            size_t out = 0;
+            for (const T &i : v)
+                out = h(out ^ ht(i));
+            return out;
         }
-        return static_cast<size_t>(r);
-    }
-};
+    };
+    template<class T, class S>
+    struct hash<pair<T, S>> {
+        size_t operator()(const pair<T, S> &p) const {
+            hash<T> ht; hash<S> hs; hash<size_t> h;
+            return hash<size_t>(ht(p.x) ^ h(hs(p.y)));
+        }
+    };
+}
