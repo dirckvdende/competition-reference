999223329f989726efedda82b8849f2964021ddf
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-10-27 17:31:32 +0100

    Replaced modMul with faster variant

--- src/math/modmul.cc
+++ src/math/modmul.cc
@@ -1,11 +1,12 @@
 // Determines x * y % n without risking overflow issues
+// x, y and n need to be in the range [0, n)
 ll modMul(ll x, ll y, ll n) {
+    ll step = (1ll << 61) / n;
     ll res = 0;
     while (y > 0) {
-        if (y % 2 == 1)
-            res = (res + x) % n;
-        x = (x * 2) % n;
-        y /= 2;
+        res = (res + x * (y % step)) % n;
+        x = (x * step) % n;
+        y /= step;
     }
     return res;
 }
