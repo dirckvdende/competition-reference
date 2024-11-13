a8e4d85e0e962fafdf7fe0832009ce2f122551ac
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-11-13 13:43:17 +0100

    #22 - Updated Aho-Corasick code

--- src/string/ahocorasick.cc
+++ src/string/ahocorasick.cc
@@ -1,115 +1,113 @@
-// Data structure for all operations needed for the
-// Aho-Corasick algorithm
+// NOTE: Duplicate patterns will be ignored!
+// NOTE: Doesn't allow for adding patterns after
+// construction!
 struct AhoCorasick {
-    // Store list of patterns separately
-    vector<string> patt;
-    const unsigned char base; const ll maxChars;
-    // Output function: used to check if current state
-    // accepts prefix strings
-    vvi out;
-    // Failure function, goto function
-    vi f; vvi g;
-    // Append an empty state
-    void add() {
-        out.pb({});
-        f.pb(-1);
-        g.pb(vi(maxChars + 1, -1));
+    // Base character offset and character range
+    const char base; const ll range;
+    // Standard trie links, and (parent node, char
+    // index)
+    vvi trie; vii parent;
+    // Blue and green edges, accepted word index or -1
+    vi blue, green; vi accepted;
+    AhoCorasick(const vector<string> &pat, char base,
+    char end) : base(base), range(ll(end) - base + 1) {
+        addNode(); // Add root node
+        createTrie(pat); createBlue(); createGreen();
     }
-    // Construct trie from multiple patterns. Give the
-    // min and max character as parameters.
-    AhoCorasick(const vector<string> &patt, unsigned
-    char mn = 'a', unsigned char mx = 'z') :
-    patt(patt), base(mn), maxChars(mx - mn + 1) {
-        // Add initial state
-        add();
-        // Step 1: Construct goto function, which is to
-        // construct a Trie
-        REP(i, sz(patt)) {
-            const string &word = patt[i];
-            ll cur = 0;
-            REP(j, sz(word)) {
-                ll ch = word[j] - base;
-                if (g[cur][ch] == -1)
-                    g[cur][ch] = sz(out), add();
-                cur = g[cur][ch];
+    // Add empty node to the automaton and return index
+    ll addNode() {
+        accepted.pb(-1); trie.pb(vi(range, -1));
+        blue.pb(-1); green.pb(-1); parent.pb({-1, -1});
+        return sz(blue) - 1;
+    }
+    // Construct trie and mark accepted patterns
+    void createTrie(const vector<string> &pat) {
+        REP(i, sz(pat)) {
+            ll j = 0;
+            for (char c : pat[i]) {
+                if (trie[j][c - base] == -1) {
+                    ll v = addNode();
+                    parent[v] = {j, c - base};
+                    trie[j][c - base] = v;
+                }
+                j = trie[j][c - base];
             }
-            // Mark current state as accepting for this
-            // specific word
-            out[cur].pb(i);
+            accepted[j] = i;
         }
-        // All characters that do not have an edge from
-        // the root will get a goto to the root
-        REP(ch, maxChars)
-            if (g[0][ch] == -1)
-                g[0][ch] = 0;
-        // Step 2: Build failure function
-        // Failure function is calculated in using BFS
-        queue<ll> q;
-        // Iterate over possible single-character
-        // inputs
-        REP(ch, maxChars)
-            if (g[0][ch] != 0)
-                f[g[0][ch]] = 0, q.push(g[0][ch]);
-        // This vector is used to keep track of double
-        // merge values:
-        vector<bool> merged(sz(patt), false);
-        // Use queue to iterate over states using BFS
+    }
+    // Create blue edges to longest strict suffix
+    // Uses BFS and blue edges of parent
+    void createBlue() {
+        queue<ll> q; q.push(0);
         while (!q.empty()) {
             ll cur = q.front(); q.pop();
-            // Determine failure function if goto
-            // function is not defined for character
-            REP(ch, maxChars + 1) if (g[cur][ch] != -1)
-            {
-                // Find failure state of removed state
-                ll fail = f[cur];
-                // Find the deepest node labeled by
-                // proper suffix of string from root to
-                // current state
-                while (g[fail][ch] == -1)
-                    fail = f[fail];
-                fail = g[fail][ch];
-                f[g[cur][ch]] = fail;
-                // Merge output values:
-                // Step A: Mark all values that are
-                // already in output value
-                for (ll v : out[g[cur][ch]])
-                    merged[v] = true;
-                // Step B: Add values for out[fail]
-                // that have not yet been added
-                for (ll v : out[fail])
-                    if (!merged[v])
-                        out[g[cur][ch]].pb(v);
-                // Step C: Unmark all values for next
-                // iteration
-                for (ll v : out[g[cur][ch]])
-                    merged[v] = false;
-                // Insert child node to queue
-                q.push(g[cur][ch]);
+            REP(i, range)
+                if (trie[cur][i] != -1)
+                    q.push(trie[cur][i]);
+            if (parent[cur].x <= 0) {
+                blue[cur] = 0;
+                continue;
             }
+            // Char that points at current node
+            ll c = parent[cur].y;
+            // Follow blue edge of parent from current
+            // node
+            ll p = blue[parent[cur].x];
+            while (p > 0 && trie[p][c] == -1)
+                p = blue[p];
+            blue[cur] = trie[p][c] == -1 ? 0 :
+            trie[p][c];
         }
     }
-    // Find the next state in the automaton given
-    // current state and input
-    ll nextState(ll cur, char c) {
-        ll res = cur;
-        // Use failure function if goto is not defined
-        while (g[res][c - base] == -1)
-            res = f[res];
-        return g[res][c - base];
-    }
-    // Find stored patterns in a string. Returns a
-    // vector where every entry contains all starting
-    // positions of one of the pattern words.
-    vvi find(const string &txt) {
-        ll cur = 0;
-        vvi res(sz(patt), vi{});
-        // Simulate automaton, looping over all chars
-        REP(i, sz(txt)) {
-            cur = nextState(cur, txt[i]);
-            // No match found in this state:
-            for (ll j : out[cur])
-                res[j].pb(i - sz(patt[j]) + 1);
+    // Create green edges to longest accepted suffix
+    // Uses BFS and blue edges
+    void createGreen() {
+        queue<ll> q; q.push(0);
+        while (!q.empty()) {
+            ll cur = q.front(); q.pop();
+            REP(i, range)
+                if (trie[cur][i] != -1)
+                    q.push(trie[cur][i]);
+            if (blue[cur] == -1)
+                continue;
+            if (accepted[blue[cur]] != -1)
+                green[cur] = blue[cur];
+            else
+                green[cur] = green[blue[cur]];
         }
-        return res;
+    }
+    // Returns a list of (end index, pat. index)
+    vii match(const string &s) {
+        ll cur = 0; vii out;
+        REP(i, sz(s)) {
+            ll c = ll(s[i]) - base;
+            // Move to next node
+            if (cur > 0 && trie[cur][c] == -1)
+                cur = blue[cur];
+            if (trie[cur][c] == -1)
+                cur = 0;
+            else
+                cur = trie[cur][c];
+            DBG(cur);
+            // NOTE: Option to return only longest
+            // match per index
+            #if ONLY_LONGEST_SUFFIX
+                if (accepted[cur] != -1)
+                    out.pb({i, accepted[cur]});
+                else if (green[cur] != -1)
+                    out.pb({i, accepted[green[cur]]});
+            #else
+                if (accepted[cur] != -1)
+                    out.pb({i, accepted[cur]});
+                ll j = cur;
+                while (green[j] != -1) {
+                    // green[j] points to next accepted
+                    // suffix
+                    j = green[j];
+                    out.pb({i, accepted[j]});
+                }
+            #endif
+        }
+        return out;
     }
 };
