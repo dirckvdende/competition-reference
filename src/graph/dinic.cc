c31fef529182185856b0169f5cd2e78b926d7ad5
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2024-11-17 16:34:02 +0100

    #30 - Improved performance of Dinic and added hash comment

--- src/graph/dinic.cc
+++ src/graph/dinic.cc
@@ -1,3 +1,10 @@
+// Special pair hashing
+struct DinicHash {
+    hash<ll> h;
+    size_t operator()(const ii &p) const {
+        return h(p.x + (p.y << 31));
+    }
+};
 // Object to run Dinic's algorithm
 struct Dinic {
     // Size of the graph
@@ -6,7 +13,7 @@ struct Dinic {
     // edges included
     vvi adj;
     // Total and residual capacity
-    unordered_map<ii, ll> cap, res;
+    unordered_map<ii, ll, DinicHash> cap, res;
     // Current level of each node
     vi level;
     // Source and sink nodes
@@ -17,15 +24,21 @@ struct Dinic {
     // Input the graph as an adjacency list
     Dinic(const vector<vii> &G, ll s, ll t) : n(sz(G)),
     adj(n), s(s), t(t), visited(n), pruned(n) {
+        // For performance: determine approx. number of
+        // entries in hash tables
+        ll tot = 0;
+        REP(i, n) tot += sz(G[i]);
+        cap.reserve(tot * 2); res.reserve(tot * 2);
         // Construct adjacency lists, set capacities
-        REP(i, n) for (const ii &e : G[i]) {
-            if (cap.find({i, e.x}) == cap.end()) {
-                cap[{i, e.x}] = cap[{e.x, i}] =
-                res[{i, e.x}] = res[{e.x, i}] = 0;
-                adj[i].pb(e.x), adj[e.x].pb(i);
+        REP(i, n)
+            for (const ii &e : G[i]) {
+                if (cap.find({i, e.x}) == cap.end()) {
+                    cap[{i, e.x}] = cap[{e.x, i}] =
+                    res[{i, e.x}] = res[{e.x, i}] = 0;
+                    adj[i].pb(e.x), adj[e.x].pb(i);
+                }
+                cap[{i, e.x}] += e.y;
             }
-            cap[{i, e.x}] += e.y;
-        }
     }
     // Find the levels of all nodes, given residual
     // graph
@@ -48,7 +61,8 @@ struct Dinic {
     // Find a blocking path using DFS, returns max-flow
     // of path if it exists, otherwise 0
     ll findPath(ll i, vi &path, ll flow) {
-        visited[i] = true; path.pb(i);
+        visited[i] = true;
+        path.pb(i);
         if (i == t)
             return flow;
         for (ll e : adj[i]) {
@@ -75,7 +89,8 @@ struct Dinic {
             vi path; ll r;
             fill(all(pruned), false);
             fill(all(visited), false);
-            while (r = findPath(s, path, LLONG_MAX)) {
+            while ((r = findPath(s, path, LLONG_MAX)))
+            {
                 REP(i, sz(path) - 1) {
                     res[{path[i], path[i + 1]}] += r;
                     res[{path[i + 1], path[i]}] -= r;
--- src/misc/hash.cc
+++ src/misc/hash.cc
@@ -1,8 +1,8 @@
 namespace std {
     template<class T>
     struct hash<vector<T>> {
+        hash<T> ht; hash<size_t> h;
         size_t operator()(const vector<T> &v) const {
-            hash<T> ht; hash<size_t> h;
             size_t out = 0;
             for (const T &i : v)
                 out = h(out ^ ht(i));
@@ -11,9 +11,9 @@ namespace std {
     };
     template<class T, class S>
     struct hash<pair<T, S>> {
+        hash<T> ht; hash<S> hs; hash<size_t> h;
         size_t operator()(const pair<T, S> &p) const {
-            hash<T> ht; hash<S> hs; hash<size_t> h;
-            return hash<size_t>(ht(p.x) ^ h(hs(p.y)));
+            return h(ht(p.x) ^ h(hs(p.y)));
         }
     };
 }
